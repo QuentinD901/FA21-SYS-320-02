2abd430ff71c7d88d82ece52160305a9085b5e8f
QuentinD901 <59927935+QuentinD901@users.noreply.github.com>
2021-09-20 16:08:12 -0400

    Run to watch video on Windows

---
+++ Week03/homework/video-windows.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Storyline: Open Video
+
+#Link if script fails: https://champlain.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=cb9d9f7f-8f99-4f94-83f3-ada601358820
+
+
+#Opens Video
+start https://champlain.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=cb9d9f7f-8f99-4f94-83f3-ada601358820
