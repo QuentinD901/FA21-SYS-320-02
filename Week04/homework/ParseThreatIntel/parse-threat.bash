4cfe31842360093a8d5fedcb6ba15926c56f8b5e
QuentinD901 <quentin.degiorgio@mymail.champlain.edu>
2021-09-29 16:37:46 -0400

    unfinished threat parsing

--- Week04/homework/ParseThreatIntel/parse-threat.bash
+++ Week04/homework/ParseThreatIntel/parse-threat.bash
@@ -14,7 +14,7 @@ then
     read the_overwrite
     if [[ "${the_overwrite}" = "N" || "${the_overwrite}" == "" ]]
     then
-        echo "Exit..."
+        echo "Keeping Existing Emerging Threats File"
     elif [[ "${the_overwrite}" == "y" ]]
     then
         echo "Downloading the Emerging Threats File..."
@@ -38,19 +38,21 @@ fi
 egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.0/[0-9]{1,2}' /tmp/emerging-drop.suricata.rules | sort -u | tee badIPs.txt
 
 # Create a Firewall Ruleset Based on Version
-while getopts 'hicnwm' OPTION ; do
+while getopts 'hicwm' OPTION ; do
 
-    case "$OPTION" in
+    case "${OPTION" in
 
         i) iptables=${OPTION}
         ;;
-        a) u_add=${OPTION}
+        c) cisco=${OPTION}
         ;;
-        u) t_user=${OPTARG}
+        w) winfirewall=${OPTION}
+        ;;
+        m) macosx=${OPTION}
         ;;
         h)
             echo ""
-            echo "Usage: $(basename $0) [-a]|[-d] -u username"
+            echo "Usage: $(basename $0) [-i]|[-c][-w][-m]"
             echo ""
             exit 1
         ;;
@@ -63,14 +65,50 @@ while getopts 'hicnwm' OPTION ; do
 done
 
 
+# Check to see if switches are empty throw an error
+if [[ (${iptables} == "" && ${cisco} == "" && ${winfirewall} == "" && ${macosx} == "") ]]
+then
+    echo "Please specify one or more options: -i -c -w -m"
+fi
+
+#checks to see if -i is specified
+if [[ ${iptables} ]]
+then
+    echo "Creating Inbound Drop Rule for IP-Tables"
+    for eachIP in $(cat badIPs.txt)
+    do
+            echo "iptables -A INPUT -s ${eachIP} -j DROP" | tee -a badIPs.iptables
+    done
+fi
+
+#checks to see if -c is specified
+if [[ ${cisco} ]]
+then
+    echo "Creating Inbound Drop Rule for Cisco Firewall"
+    for eachIP in $(cat badIPs.txt)
+    do
+            echo "iptables -A INPUT -s ${eachIP} -j DROP" | tee -a badIPs.cisco
+    done
+fi
+
+#checks to see if -w is specified
+if [[ ${winfirewall} ]]
+then
+    echo "Creating Inbound Drop Rule for Windows Firewall"
+    for eachIP in $(cat badIPs.txt)
+    do
+            echo "iptables -A INPUT -s ${eachIP} -j DROP" | tee -a badIPs.winfirewall
+    done
+fi
 
 
-
-
-
-
-for eachIP in $(cat badIPs.txt)
-do
-        echo "iptables -A INPUT -s ${eachIP} -j DROP" | tee -a badIPs.iptables
-done
+#checks to see if -m is specified
+if [[ ${macosx} ]]
+then
+    echo "Creating Inbound Drop Rule for Mac OS X"
+    for eachIP in $(cat badIPs.txt)
+    do
+            echo "block in from ${eachIP} to any" | tee -a badIPs.macosx
+    done
+fi
 
