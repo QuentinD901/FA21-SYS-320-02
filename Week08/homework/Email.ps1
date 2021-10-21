016e9fec42147dacf22aef299cb9ab3820a12ad7
QuentinD901 <59927935+QuentinD901@users.noreply.github.com>
2021-10-21 13:55:05 -0400

    Completed W8 Assignment :)

---
+++ Week08/homework/Email.ps1
@@ -0,0 +1,16 @@
+﻿# Storyline: Send an Email
+
+# Email Body
+$msg = "Top of the mornin'"
+
+# Write to Screen
+Write-Host -BackgroundColor Red -ForegroundColor White $msg
+
+# From Address
+$fmail = "quentin.degiorgio@mymail.champlain.edu"
+
+# Reciever Address
+$remail = "deployer@csi-web"
+
+# Send Email
+Send-MailMessage -From $fmail -To $remail -Subject "Greetings" -Body $msg -SmtpServer 192.168.6.71
---
+++ Week08/homework/Logs.ps1
@@ -0,0 +1,16 @@
+﻿# Storyline: Review the Security Event Logs
+
+# List all the Win Event Logs
+Get-EventLog -list
+
+# Prompt allowing user to select the Log
+$readLog = Read-Host -Prompt "Please select a log to review from the above list"
+
+# Prompt allowing user to provide a Search Phrase or Word(s)
+$readPhrase = Read-Host -Prompt "Please provide a Word or Phrase to filter $readLog Logs"
+ 
+# Directory Info
+$myDir = "C:\Users\Quentin DeGiorgio\Desktop\Champlain\SYS-320\$readLog Logs.csv"
+
+# Print the results
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$readPhrase*"} | Export-Csv -NoTypeInformation -Path $myDir
