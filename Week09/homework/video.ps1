c672bafe88dda4efe7f4397989cde7a911bb0432
QuentinD901 <59927935+QuentinD901@users.noreply.github.com>
2021-11-03 20:58:47 -0400

    Update video.ps1

--- Week09/homework/video.ps1
+++ Week09/homework/video.ps1
@@ -1,1 +1,5 @@
+# StoryLine: Open Video's Link
 
+# URL Incase Script Fails: https://champlain.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=c0a5e18e-155f-4673-91ab-add6000e49d0
+
+Start-Process https://champlain.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=c0a5e18e-155f-4673-91ab-add6000e49d0
