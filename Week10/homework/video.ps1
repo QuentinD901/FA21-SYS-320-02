2f547e2f07a3689359da745ba763498fe17c2235
QuentinD901 <59927935+QuentinD901@users.noreply.github.com>
2021-11-10 15:12:00 -0500

    Completed

---
+++ Week10/homework/video.ps1
@@ -0,0 +1,5 @@
+# StoryLine: Open Video's Link
+
+# URL Incase Script Fails: https://champlain.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=f94d2f45-ecf4-4868-a8f1-addc014c5a85
+
+Start-Process https://champlain.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=f94d2f45-ecf4-4868-a8f1-addc014c5a85
