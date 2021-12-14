dacf32062fd0230e63ffba8d7f30374c85f88b20
QuentinD901 <59927935+QuentinD901@users.noreply.github.com>
2021-12-14 13:57:54 -0500

    Create video.ps1

---
+++ Week15/homework/video.ps1
@@ -0,0 +1,5 @@
+# StoryLine: Open Video's Link
+
+# URL Incase Script Fails: https://champlain.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=5ff30c92-5f50-44cf-a5b2-adfe01374db1
+
+Start-Process https://champlain.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=5ff30c92-5f50-44cf-a5b2-adfe01374db1
